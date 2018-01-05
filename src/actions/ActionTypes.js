d206a2248dea313279d15686f7223f70a9f0bd70
Paulo Teixeira <paulo.teixeira_sub@noesis.pt>
2018-01-05 16:33:43 +0000

    commit

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,7 @@
+export const CREATE_NEW_BOARD = "CREATE_NEW_BOARD"
+export const FETCH_BOARDS = "FETCH_BOARDS"
+export const FETCH_BOARD_DATA = "FETCH_BOARDS"
+
+export const PENDING = "_PENDING"
+export const FULFILLED = "_FULFILLED"
+export const ERROR = "_ERROR"
